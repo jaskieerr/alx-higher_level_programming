17dca546942a6b9f4f39fe12b1fab2e12bf8bc43
jaskieerr <nizar.zairat@gmail.com>
2023-10-03 09:53:00 -0700

    wtff

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(97, 123):
     if c is not (ord('q')) and c is not (ord('e')):
-        print('{}'.format(chr(c)), end='')
+        print('{}'.format(chr(c)), end="")
