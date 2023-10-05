b16350653b77ade74e6dd97af2fa684a6599b3a0
jaskieerr <nizar.zairat@gmail.com>
2023-10-05 08:33:49 -0700

    simple calcul

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import sub, add, mul, div
     import sys
-    
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if sys.argv[2] == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif sys.argv[2] == '-':
