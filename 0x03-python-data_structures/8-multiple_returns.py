9cb3e4fd0b0de500bdc1b14c3f1dd3085a74a840
jaskieerr <nizar.zairat@gmail.com>
2023-10-08 15:55:48 -0700

    len

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
+    length = len(sentence) if length > 0 else None
     first_char = sentence[0]
     return length, first_char
