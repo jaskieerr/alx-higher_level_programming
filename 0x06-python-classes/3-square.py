b6b7a0c8935f2e41f5a4d21b17f639dffe06088a
jaskieerr <nizar.zairat@gmail.com>
2023-10-24 15:28:02 -0700

    fingers

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,7 +7,6 @@ class Square:
 
     def __init__(self, size=0):
         '''Initializing size to 0'''
-        
         if type(size) is int:
             if size < 0:
                 raise ValueError('size must be >= 0')
@@ -15,7 +14,6 @@ class Square:
                 self.__size = size
         else:
             raise TypeError('size must be an integer')
-
 
     def area(self):
         '''Return the current area of the square.'''
