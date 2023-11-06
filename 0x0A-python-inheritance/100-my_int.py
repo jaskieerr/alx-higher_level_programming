06e77aefab7f69196afd4a20e3c5be731ecc1bd6
jaskieerr <nizar.zairat@gmail.com>
2023-11-06 07:41:18 -0800

    hh

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''shbang'''
+
+class MyInt(int):
+    '''rebel rebell by david bowie'''
+    def __new__(cls, *args, **kwargs):
+        '''creating instance'''
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        '''reverse op'''
+        return int(self) != other
+
+    def __ne__(self, other):
+        '''anotherone'''
+        return int(self) == other
