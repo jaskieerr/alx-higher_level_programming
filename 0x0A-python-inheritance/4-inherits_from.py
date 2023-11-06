ea16849d79b9e9460b055c9e63687296bda90c37
jaskieerr <nizar.zairat@gmail.com>
2023-11-06 06:29:50 -0800

    pycode

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     '''u already know amigoo '''
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
