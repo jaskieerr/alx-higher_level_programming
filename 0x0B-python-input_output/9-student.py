afe72259eafbea4729acd75f997dd420bcf809ff
jaskieerr <nizar.zairat@gmail.com>
2023-11-07 02:49:35 -0800

    myrath

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''shebang'''
+
+
+class Student:
+    '''creatin student class'''
+    def __init__(self, first_name, last_name, age):
+        '''initin it'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''rtfmmm'''
+        return self.__dict__
