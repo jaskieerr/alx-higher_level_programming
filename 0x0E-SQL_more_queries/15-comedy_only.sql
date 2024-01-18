e99fce0bf1c6dfdc592d3b6ccaea778723582f81
jaskieerr <nizar.zairat@gmail.com>
2024-01-17 17:58:48 -0800

    jesuschristt

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,7 @@
--- just comedy
-SELECT tv_shows.title, tv_genres.name FROM tv_shows
-    LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-    LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+-- just comedy tv show
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title, tv_genres.name;
