523b850212f5711f54a3822a8391af34bdbd4af5
jaskieerr <nizar.zairat@gmail.com>
2024-02-12 15:36:27 -0800

    wtf

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+
 const { argv } = require('process');
 const argcc = argv.length;
+
 if (argcc <= 2) {
   console.log('No argument');
 } else if (argcc === 3) {
   console.log('Argument found');
 } else {
-  console.log('Argument found');
+  console.log('Too many arguments');
 }
