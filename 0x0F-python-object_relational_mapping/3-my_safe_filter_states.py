8e93eb5d7542a0f918251c6416776cf187d1b9ca
jaskieerr <nizar.zairat@gmail.com>
2024-02-18 13:47:08 -0800

    not hacker man

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''arizona '''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    db_conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
+
+    cr = db_conn.cursor()
+
+    cr.execute("SELECT * FROM states WHERE name LIKE %s", (sys.argv[4], ))
+
+    for row in cr:
+        print(row)
