defd7cad082ffa491d8fd85e1e041a5937bc439f
jaskieerr <nizar.zairat@gmail.com>
2024-02-29 15:45:56 -0800

    "$message"

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sends GET request toURL, displays the body of the response
+curl -sL "$1"
---
+++ 0x10-python-network_0/push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git add .
+
+message = "$1"
+
+git commit -m '"$message"'
+
+git push
