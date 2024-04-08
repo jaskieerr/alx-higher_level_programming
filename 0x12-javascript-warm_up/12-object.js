531fc9a9d66f1c33658f563ea798c48ee6d0f18d
jaskieerr <nizar.zairat@gmail.com>
2024-04-08 15:11:06 -0700

    ss

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+#!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+myObject.value = 89;
+
+console.log(myObject);
