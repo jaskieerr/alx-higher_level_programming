4ee7745af5aaac67e77bc15b2816b2d120e7c667
jaskieerr <nizar.zairat@gmail.com>
2024-04-09 13:58:21 -0700

    fixin

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@
 const ParentSquare = require('./5-square');
 
 class Square extends ParentSquare {
-  charPrint(c = 'X') {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
