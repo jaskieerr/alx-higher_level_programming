d0cb9fa2f57c0263d4f0893f419a9c1a3e9ad4a5
jaskieerr <nizar.zairat@gmail.com>
2024-04-09 13:48:44 -0700

    te

---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+js scopes and closures
---
+++ 0x13-javascript_objects_scopes_closures/cr.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+touch 1-rectangle.js
+touch 2-rectangle.js
+touch 3-rectangle.js
+touch 4-rectangle.js
+touch 5-square.js
+touch 6-square.js
+touch 7-occurrences.js
+touch 8-esrever.js
+touch 9-logme.js
+touch 10-converter.js
