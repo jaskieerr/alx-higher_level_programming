673cd49ec0e6e3eba0123a2fa2394c95d787fea7
jaskieerr <nizar.zairat@gmail.com>
2024-05-21 18:22:04 -0700

    damn typo

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-url =  process.arfv[2];
+url =  process.argv[2];
 path = process.argv[3];
 request(url).pipe(fs.createWriteStream(path));
