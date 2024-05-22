349882717a2c762e813d93b3b621c35fc13b8e93
jaskieerr <nizar.zairat@gmail.com>
2024-05-21 18:28:29 -0700

    aaahhhh

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const request = require('request');
 
-url =  process.argv[2];
-path = process.argv[3];
+const url =  process.argv[2];
+const path = process.argv[3];
 
 request(url).pipe(fs.createWriteStream(path));
